39dd405ee64e7f07e2694d1a223760d5597d33bd
Eunhyeok Choi <109642542+harryPG0911@users.noreply.github.com>
2022-07-20 16:27:39 +0900

    Update Sonic_bot_git.py

--- Sonic_bot_git.py
+++ Sonic_bot_git.py
@@ -29,15 +29,8 @@ async def on_message(message):
         await message.channel.send("나 불렀어?")
     
     
-
-    
-    if message.content == '소닉 버전 확인':
-        embed = discord.Embed(title="Sonic Discord Bot Version", description="Now Version is" + now_version, color=0x62c1cc) # Embed의 기본 틀(색상, 메인 제목, 설명)을 잡아줍니다
-        embed.set_footer(text="Harry_STH") # 하단에 들어가는 조그마한 설명을 잡아줍니다
-        await message.channel.send(embed=embed) # embed를 포함 한 채로 메시지를 전송합니다.
     
 
-        #출처: https://code-200.tistory.com/62 [CODE 200]
         
     if message.content.startswith("소닉 음챗 컴온"):
        await message.author.voice.channel.connect()
