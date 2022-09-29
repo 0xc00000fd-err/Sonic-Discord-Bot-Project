d803c0d2a53fa6202c6a28df77b80f5b4d78d145
Eunhyeok Choi <109642542+harrydv0911@users.noreply.github.com>
2022-09-30 00:07:14 +0900

    간결하게 수정

--- Sonic_bot_git.py
+++ Sonic_bot_git.py
@@ -10,36 +10,32 @@ async def on_ready():
     
     
     await client.change_presence(status=discord.Status.online, activity = discord.Activity(type=discord.ActivityType.listening, name="Green Hill zone"))
-    print("안녕! 난 소닉, 소닉 더 헤지혹이야!"))
-    
-    
-    
-    """Game: Sonic the Hedgehog이라고 설정"""
-    
-        
-        
+
+"""디스코드 봇 상태 설정하기"""
+
 @client.event
 
 async def on_message(message):
-    if message.content == '소닉':
-        await message.channel.send("나 불렀어?")
+    """메세지 입력 반응 테스트"""
+    if message.content == '안녕':
+        await message.channel.send("안녕!")
     
     
     
 
-        
-    if message.content.startswith("소닉 음챗 컴온"):
+    """음성 채널 참여 테스트"""    
+    if message.content.startswith("voice channal - in"):
        await message.author.voice.channel.connect()
-       await message.channel.send("알겠어!")
+       await message.channel.send("Ok!")
        
-    
-    if message.content.startswith("소닉 ㅃ"):
+    """음성 채널 아웃 """
+    if message.content.startswith("voice channal - out"):
         for vc in client.voice_clients:
             if vc.guild == message.guild:
                 voice=vc
         
         await voice.disconnect()
-        await message.channel.send("알겠어!")
+        await message.channel.send("..done!")
         
         
 
